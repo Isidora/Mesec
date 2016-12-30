142c4aa9a0c01dfe13fdb67bbd73e39660239d2d
Isidora <isidora.rapajic97@gmail.com>
2016-12-31 00:18:52 +0100

    prva verzija

---
+++ moon.py
@@ -0,0 +1,12 @@
+def moon():
+    masa_projektila = 1000 #kg
+    poluprecnik_projektila = 1 #m
+    pocetna_sila = 4*10**6b #N
+    vreme = 30 #s
+    #pocetna sila deluje na projektil 30 s
+    orbita_meseca = 384405000 #m (to je zapravo udaljenost o zemlji)
+    poluprecnik_zemlje = 6378000 #m
+    masa_zemlje = 6*10**24 #kg
+    poluprecnik_meseca = 1738000 #m
+    masa_meseca = 7.3*10**22 #kg
+    
